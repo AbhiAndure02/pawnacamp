3529f9cd5d4cafb8e0a8d02bd7c30989d255c367
AbhiAndure02 <abhiandure123@gmail.com>
2024-12-22 12:11:08 +0530

    update

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 
 export async function db(){
 try{
-    await mongoose.connect(process.env.MONGO_URL)
+    await mongoose.connect("mongodb+srv://abhiandure123:abhi@pawnacamp.jrp2q.mongodb.net/?retryWrites=true&w=majority&appName=pawnacamp"
+    )
     .then(()=>
     console.log('Connected to MongoDB'))
     .catch(err=>console.log('Error connecting to MongoDB'))
